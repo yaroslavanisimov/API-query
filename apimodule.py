2d51aa6e8ff72490cb4281d3e8c9b63f773555a1
Yaro <yaroslavanisimow@gmail.com>
2022-02-26 20:04:11 +0100

    admin Thread-proc engine

--- apimodule.py
+++ apimodule.py
@@ -1,11 +1,14 @@
 # This is a sample Python script.
-
+import concurrent.futures
 import logging
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import threading
+import time
 from typing import TypeVar
 
 import jproperties as jproperties
+from django.db.migrations import executor
 from flask import Flask, jsonify
 import testmodule
 from dbmodule import Task
@@ -52,9 +55,34 @@ def get_fake_create():
 
 @app.route(config.get('api-path').data + '/ext-loader', methods=['GET'])
 def perform_external_function():
-    logging.info("perform_external_function invoked")
-    getattr(testmodule, config.get('ext.function-name').data)('test_message')
+    while True:
+        logging.info("perform_external_function invoked")
+    # getattr(testmodule, config.get('ext.function-name').data)('test_message')
     return jsonify({'function-name': config.get('ext.function-name').data})
+
+
+@app.route(config.get('api-path').data + '/proc-loader', methods=['GET'])
+def perform_proc_function():
+    task_back = threading.Thread(target=back_thread)
+    task_back.start()
+    logging.info("perform_process_function invoked")
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=config.get('app.max.workers')) as executor:
+    task_th = threading.Thread(target=task_thread)
+    task_th.start()
+    # getattr(testmodule, config.get('ext.function-name').data)('test_message')
+    return jsonify({'function-name': config.get('ext.function-name').data})
+
+
+def task_thread():
+    while True:
+        time.sleep(3)
+        threading.Thread(getattr(testmodule, config.get('ext.function-name').data)('test_message')).start()
+
+
+def back_thread():
+    while True:
+        time.sleep(3)
+        print('main')
 
 
 # możliwosć przekazania nazwy funkcji którą chce się wykonać
